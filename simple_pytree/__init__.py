8419d6f84dc4d4b58c256ec4608f205a48e74edb
Cristian Garcia <cgarcia.e88@gmail.com>
2023-03-17 10:18:01 -0500

    Revert "Add PytreeRef + ref_field (#3)" (#6)
    This reverts commit d0a4248443020ccf2f46a552346d47bc3302777f.

--- simple_pytree/__init__.py
+++ simple_pytree/__init__.py
@@ -1,12 +1,3 @@
 __version__ = "0.1.5"
 
 from .pytree import Pytree, field, static_field
-from .ref import (
-    PytreeRef,
-    Ref,
-    RefField,
-    clone_references,
-    cross_barrier,
-    incremented_ref,
-    ref_field,
-)
